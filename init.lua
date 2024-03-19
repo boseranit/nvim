d302f9c090d4fd181c639d3192a15243d1ba3e9f
Ranit Bose <59643631+boseranit@users.noreply.github.com>
2024-03-20 03:14:44 +1100

    converted into lua
    removed colour scheme since it doesn't belong in this folder anyway. have created a backup of vim-solarized8.
    maybe eventually convert vim-plug to packer.

---
+++ init.lua
@@ -0,0 +1,97 @@
+local PATH = "$HOME/AppData/Local/nvim/plugged"
+local Plug = vim.fn['plug#']
+
+-- vim-plug stuff. convert to Packer for lua implementation
+vim.call('plug#begin', PATH)
+	Plug 'lervag/vimtex'
+	-- Plug 'KeitaNakamura/tex-conceal.vim'
+
+	Plug 'scrooloose/nerdtree'
+	Plug 'bling/vim-airline'
+
+	Plug 'SirVer/ultisnips'
+
+	Plug 'tmsvg/pear-tree'
+
+	-- Autocomplete with Deoplete
+	Plug 'Shougo/deoplete.nvim'
+vim.call('plug#end')
+-- 'filetype plugin indent on' already called in plug#end()
+
+-- Tabs and spaces
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.autoindent = true 
+vim.opt.textwidth = 80
+vim.opt.wrap = true
+vim.opt.colorcolumn = "81" -- +1 is convention
+
+local function map(mode, lhs, rhs, opts)
+local options = {noremap = true}
+    if opts then options = vim.tbl_extend('force', options, opts) end
+    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+map('n', '<CR>', '<cmd>noh<CR>')    -- clears highlights
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.mouse = 'a'
+vim.opt.background = 'dark'
+-- colorscheme cloned in Neovim\share\nvim\runtime\pack\dist\opt
+-- https://github.com/lifepillar/vim-solarized8 tag v1.3.0 
+-- v1.4.0 causes vimtex bug with inline imap and snippets
+vim.cmd('colorscheme solarized8_high')
+vim.opt.termguicolors = true
+vim.cmd("syntax on")
+
+
+-- Vimtex
+vim.g.tex_flavor = 'latex'
+-- to make a build directory for latex files
+vim.g.vimtex_compiler_latexmk = {
+        build_dir = "build",
+        options = {
+          "-shell-escape",
+          "-verbose",
+          "-file-line-error",
+          "-interaction=nonstopmode",
+          "-synctex=1",
+        },
+      }
+vim.g.vimtex_compiler_progname = 'nvr'
+vim.g.vimtex_view_general_viewer = 'SumatraPDF'
+vim.g.vimtex_view_general_options = '-reuse-instance -forward-search @tex @line @pdf'
+vim.opt.conceallevel = 1 
+vim.g.tex_conceal = 'abdmg'
+vim.cmd('hi Conceal ctermbg=none')
+
+-- vim airline
+-- vim.g.airline_theme = 'darkness'
+vim.g.airline_powerline_fonts = true
+vim.g['airline#extensions#tabline#enabled'] = true
+vim.g['airline#extensions#nvimlsp#enabled'] = true
+-- Show filename and parent directory
+vim.g['airline#extensions#tabline#formatter'] = 'short_path'
+vim.opt.laststatus = 2
+-- we don't need mode since airline show it
+vim.opt.showmode = false
+
+-- ultisnips triggers
+vim.g.UltiSnipsSnippetDirectories = {'C:/Users/boser/AppData/Local/nvim/plugged/ultisnips','C:/Users/boser/AppData/Local/nvim/my-snippets/UltiSnips'}
+vim.g.UltiSnipsExpandTrigger="<tab>"
+vim.g.UltiSnipsJumpForwardTrigger="<tab>"
+vim.g.UltiSnipsJumpBackwardTrigger="<s-tab>"
+
+
+-- Pear Tree auto-pair
+vim.g.pear_tree_pairs = {
+  ["("] = { closer = ")" },
+  ["["] = { closer = "]" },
+  ["{"] = { closer = "}" },
+  ["'"] = { closer = "'" },
+  ['"'] = { closer = '"' },
+  ['/*'] = { closer = '*/' },
+}
+-- vim.g.pear_tree_smart_openers = 1
+vim.g.pear_tree_smart_closers = 1
+vim.g.pear_tree_smart_backspace = 1
